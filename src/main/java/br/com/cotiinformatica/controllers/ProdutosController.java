8aab9446d039c045ef42d5ad5f73c81148357a8a
Renato Areas <rsareas93@gmail.com>
2023-04-10 22:01:29 -0300

    criando estrutura para o CQRS

--- src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
@@ -1,34 +1,53 @@
 package br.com.cotiinformatica.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cotiinformatica.dtos.commands.ProdutoCreateCommand;
+import br.com.cotiinformatica.dtos.commands.ProdutoUpdateCommand;
+import br.com.cotiinformatica.dtos.queries.ProdutosDto;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutosController {
-	
+
 	@PostMapping
-	public  void post() {
-		//TODO
+	public ResponseEntity<Object> post(@RequestBody ProdutoCreateCommand command) {
+		// TODO
+		return null;
 	}
-	
+
 	@PutMapping
-	public void put() {
-		//TODO
+	public ResponseEntity<Object> put(@RequestBody ProdutoUpdateCommand command) {
+		// TODO
+		return null;
 	}
-	
-	@DeleteMapping
-	public void delete() {
-		//TODO
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<Object> delete(@PathVariable("id") Integer id) {
+		// TODO
+		return null;
 	}
-	
+
 	@GetMapping
-	public void getAll() {
+	public ResponseEntity<List<ProdutosDto>> getAll() {
+		// TODO
+		return null;
+	}
+	
+	public ResponseEntity<ProdutosDto> getById(@PathVariable("id") Integer id){
+		
 		//TODO
+		return null;
 	}
 
 }
--- src/main/java/br/com/cotiinformatica/services/ProdutoService.java
+++ src/main/java/br/com/cotiinformatica/services/ProdutoService.java
@@ -1,8 +1,35 @@
 package br.com.cotiinformatica.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.dtos.commands.ProdutoCreateCommand;
+import br.com.cotiinformatica.dtos.queries.ProdutosDto;
 
 @Service
 public class ProdutoService {
+
+	public void create(ProdutoCreateCommand commad) {
+		// TODO
+	}
+
+	public void update(ProdutoCreateCommand commad) {
+		// TODO
+	}
+
+	public void delete(ProdutoCreateCommand commad) {
+		// TODO
+	}
+
+	public List<ProdutosDto> findAll() {
+		// TODO
+		return null;
+	}
+
+	public ProdutosDto findById(Integer id) {
+		// TODO
+		return null;
+	}
 
 }
